8c04b7f3ef3925a78f90f8f309411a3eb83b9bd1
shubham4132 <shubhamg3122134@gmail.com>
2024-02-05 23:45:26 +0530

    improving the reducer function code quality

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,8 +1,8 @@
 import { useReducer, useState } from "react";
 function reducer(state, action) {
   console.log(state, action);
-  if (action.type === "dec") return state + action.payload;
-  if (action.type === "inc") return state + action.payload;
+  if (action.type === "dec") return state - 1;
+  if (action.type === "inc") return state + 1;
   if (action.type === "setCount") return action.payload;
 }
 
@@ -19,13 +19,13 @@ export default function DateCounter() {
   const dec = function () {
     // setCount((count) => count - 1);
     // setCount((count) => count - step);
-    dispatch({ type: "dec", payload: -1 });
+    dispatch({ type: "dec" });
   };
 
   const inc = function () {
     // setCount((count) => count + 1);
     // setCount((count) => count + step);
-    dispatch({ type: "inc", payload: 1 });
+    dispatch({ type: "inc" });
   };
 
   const defineCount = function (e) {
